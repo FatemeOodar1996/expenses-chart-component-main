f7a33a4c7f69896f5f2bd850dc15d7d81616a5ed
Fateme Oodar <105479013+FatemeOodar1996@users.noreply.github.com>
2022-06-26 22:52:11 +0430

    json file

--- app.js
+++ app.js
@@ -1,38 +1,54 @@
-// import data from "./data.json";
-var options = {
-  chart: {
-    type: "bar",
-    background: "#fff",
-  },
-  //   dataLabels: {
-  //     enabled: false
-  // },
-  series: [
-    {
-      name: "money",
-      data: [30, 40, 45, 50, 49, 60, 70, 91, 125],
-    },
-  ],
-  noData: {
-    text: "Loading...",
-  },
-  xaxis: {
-    day: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
-  },
-  fill: {
-    colors: ["#F44336"],
-  },
-  dataLabels: {
-    style: {
-      colors: ["#fff"],
-    },
-  },
-};
-
-var chart = new ApexCharts(document.querySelector("#chart"), options);
-
-chart.render();
-
 fetch("./data.json")
   .then((response) => response.json())
-  .then((obj) => console.log(obj));
+  .then((arr) => {
+    const xAxis = [];
+    const yAxix = [];
+    arr.forEach((item) => {
+      xAxis.push(item.day);
+      yAxix.push(item.amount);
+    });
+    var options = {
+      chart: {
+        type: "bar",
+        background: "#fff",
+      },
+      series: [
+        {
+          data: yAxix,
+        },
+      ],
+      xaxis: {
+        type: "category",
+        categories: xAxis,
+      },
+      yaxis: {
+        labels: {
+          show: false,
+        },
+      },
+      plotOptions: {
+        bar: {
+          borderRadius: 10,
+        },
+      },
+      colors: [
+        function ({ value }) {
+          if (value == Math.max(...yAxix)) {
+            return "#7E36Af";
+          } else {
+            return "#D9534F";
+          }
+        },
+      ],
+      dataLabels: {
+        enabled: false,
+        style: {
+          colors: ["#fff"],
+        },
+      },
+    };
+
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+    chart.render();
+  });
